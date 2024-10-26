49fee352214ef28d824fd278d84ee614d7a58669
palanx <tmunizq@gmail.com>
2024-10-26 02:09:50 -0300

    tmp_text-extensions-test namespace fixed

--- Assets/Tests/PlayMode/TextMeshPro/Extensions/TMP_TextExtensionsTest.cs
+++ Assets/Tests/PlayMode/TextMeshPro/Extensions/TMP_TextExtensionsTest.cs
@@ -1,5 +1,6 @@
-﻿namespace Tests.PlayMode.TextMeshPro.Extensions
+﻿namespace CoreToolKit.Tests.PlayMode.TextMeshPro.Extensions
 {
+  // ReSharper disable once InconsistentNaming
   public class TMP_TextExtensionsTest
   {
     // TODO: Create Tests
